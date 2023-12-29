265b74ceab62d0ebfce407e184f8b83134ea48c8
Kai Breitbarth <kai.breitbarth@gmail.com>
2023-12-29 19:37:35 +0100

    adding .env

--- core/settings.py
+++ core/settings.py
@@ -8,6 +8,7 @@ from django.utils.log import DEFAULT_LOGGING
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
